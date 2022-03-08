9b28d16906bfbf878776b242b2ac88e4e1b92fef
Anant Kaul <56610952+anantkaul@users.noreply.github.com>
2022-03-08 10:12:52 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
-__author__ = "Andrea Fioraldi"
-__copyright__ = "Copyright 2017, Andrea Fioraldi"
+__author__ = "Anant Kaul"
+__copyright__ = "Copyright 2022, Anant Kaul"
 __license__ = "MIT"
-__email__ = "andreafioraldi@gmail.com"
+__email__ = "anantkaul2000@gmail.com"
 
 from setuptools import setup
 
@@ -14,8 +14,8 @@ setup(
     description='Search an exploit in the local exploitdb database by its CVE',
     author=__author__,
     author_email=__email__,
-    url='https://github.com/andreafioraldi/cve_searchsploit',
-    download_url = 'https://github.com/andreafioraldi/cve_searchsploit/archive/master.tar.gz',
+    url='https://github.com/anantkaul/cve_searchsploit',
+    download_url = 'https://github.com/anantkaul/cve_searchsploit/archive/master.tar.gz',
     package_dir={'cve_searchsploit': 'cve_searchsploit'},
     packages=['cve_searchsploit'],
     install_requires=[
